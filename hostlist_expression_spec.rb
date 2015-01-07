f87b15550e7d5ffd2eb068ebdd1c695662a15f63
udondan <deemes79@gmail.com>
2015-01-07 04:23:08 +0100

    added reverse range tests

--- hostlist_expression_spec.rb
+++ hostlist_expression_spec.rb
@@ -19,7 +19,16 @@ describe "separator - " do
       expect(hosts["#{n - 1}".to_i]).to eq "host-#{leading_zero}.com"
     }
   end
-
+  it 'numeric from 99 to 01' do
+    hosts = Array.new
+    hosts = hostlist_expression("host-[99-01].com")
+    expect(hosts.instance_of?(Array)).to be_truthy
+    1.upto(99){|n|
+      leading_zero = n.to_s.rjust(2, '0')
+      expect(hosts["#{n - 1}".to_i]).to eq "host-#{leading_zero}.com"
+    }
+  end
+  
   it 'string from a to z' do
     hosts = Array.new
     alphabet = [*'a'..'z'] # Array splat
@@ -28,7 +37,6 @@ describe "separator - " do
       expect(hosts[i]).to eq "host-#{word}.com"
     }
   end
-
   it 'string from A to Z' do
     hosts = Array.new
     alphabet = [*'A'..'Z'] # Array splat
@@ -37,4 +45,13 @@ describe "separator - " do
       expect(hosts[i]).to eq "host-#{word}.com"
     }
   end
+  it 'string from Z to A' do
+    hosts = Array.new
+    alphabet = [*'A'..'Z'] # Array splat
+    hosts = hostlist_expression("host-[Z-A].com")
+    alphabet.each_with_index {|word, i|
+      expect(hosts[i]).to eq "host-#{word}.com"
+    }
+  end
+  
 end
